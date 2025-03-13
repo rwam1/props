3ecdcd11ae26d7f2d1e2ac85d33d422fd5fa62f7
rwam1 <rwam1.nikhil.ub@gmail.com>
2025-03-13 11:31:55 +0530

    state

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,34 @@
-import React from 'react'
-import Home from './Pages/Home'
-import About from './Pages/About'
-import Contact from './Pages/Contact'
+import React from "react";
+import Home from "./Pages/Home";
+import About from "./Pages/About";
+import Contact from "./Pages/Contact";
 const App = () => {
-  const name = 'sumit'
+  const name = "sumit";
+  const data = [
+    {
+      column: "Column 1",
+      para1: "Lorem ipsum dolor sit amet, consectetur adipisicing elit...",
+      para2: "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris...",
+    },
+    {
+      column: "Column 2",
+      para1: "Lorem ipsum dolor sit amet, consectetur adipisicing elit...",
+      para2: "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris...",
+    },
+    {
+      column: "Column 3",
+      para1: "Lorem ipsum dolor sit amet, consectetur adipisicing elit...",
+      para2: "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris...",
+    },
+  ];
 
-  
-  
   return (
     <div>
-      <Home sumit={name}/>
-      <About  />
+      <Home sumit={name} data={data} />
+      <About />
       <Contact />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,42 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import Navbar from "./Navbar";
 
 const Home = (props) => {
-    const {sumit} =props
+  const { sumit, data } = props;
+  console.log(data);
+  const [name,setName] =useState(0)
+
+console.log(name);
+
   return (
     <div>
-   
       <div className="container-fluid p-5 bg-primary text-white text-center">
         <h1>My First Bootstrap Page {sumit}</h1>
         <p>Resize this responsive page to see the effect!</p>
       </div>
 
       <div className="container mt-5">
+        <button className="btn info" onClick={()=>setName("sumit")}>change name</button>
         <div className="row">
-          <div className="col-sm-4">
-            <h3>Column 1</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit...</p>
-            <p>
-              Ut enim ad minim veniam, quis nostrud exercitation ullamco
-              laboris...
-            </p>
-          </div>
-          <div className="col-sm-4">
-            <h3>Column 2</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit...</p>
-            <p>
-              Ut enim ad minim veniam, quis nostrud exercitation ullamco
-              laboris...
-            </p>
-          </div>
-          <div className="col-sm-4">
-            <h3>Column 3</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit...</p>
-            <p>
-              Ut enim ad minim veniam, quis nostrud exercitation ullamco
-              laboris...
-            </p>
-          </div>
+          {data.map((item) => {
+            return (
+              <div className="col-sm-4">
+                <h3>{item.column}</h3>
+                <p>{item.para1}</p>
+                <p>
+                  Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                  laboris...
+                </p>
+              </div>
+            );
+          })}
         </div>
       </div>
     </div>
